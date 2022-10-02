f2e16636ddafca2ed67d98ad67c4cad17ee213a9
jole84 <johanlennartsson84@gmail.com>
2022-10-02 14:22:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,18 +8,19 @@ cp /etc/apt/sources.list /etc/apt/sources.list.bak
 if [ $input == "yes" ]; then
     echo "yes"
     cp sources-unstable.list /etc/apt/sources.list
-    apt update -y
 else
     echo "no"
     cp sources.list /etc/apt/sources.list
 fi
+apt update -y
+apt upgrade -y
 
 # install packages
-apt update
 apt install gcc make perl linux-headers-amd64 bsdmainutils flatpak git firmware-linux network-manager gdebi synaptic bash-completion kde-plasma-desktop plasma-nm ark python3-pip curl ffmpeg cmus sddm-theme-debian-breeze paper-icon-theme materia-gtk-theme materia-kde
 
 # install chrome browser
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb
+apt install -f
 
 # flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo

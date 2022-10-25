ea53d19fd36a06dc26953643f3f5b160e60f6f0d
jole84 <77931011+jole84@users.noreply.github.com>
2022-10-25 20:07:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -107,6 +107,9 @@ apt install -y \
     python3-gdal \
     python3-protobuf
 
+# remove old intel driver
+apt remove xserver-xorg-video-intel
+
 # flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
